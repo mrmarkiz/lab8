7b078462d6a408e4109e4909688dafa96d30566c
mrmarkiz <maxtomusiak315@gmail.com>
2023-04-18 17:47:44 +0300

    Add project files.

---
+++ lab8/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace lab8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int choice;
+            do
+            {
+                Console.Write("Enter task to run: ");
+                int.TryParse(Console.ReadLine(), out choice);
+                switch (choice)
+                {
+                    case 1:
+                        Task1.Run();
+                        break;
+                }
+
+            } while (choice != 0);
+        }
+    }
+}
